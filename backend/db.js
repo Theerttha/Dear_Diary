b6d75a8e229103dd199cd4242569b109b4db5b7e
Theerttha <theerttha.s@gmail.com>
2025-05-13 00:34:44 +0530

    dbvariables2

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const db= mysql.createConnection({
     host:process.env.host,
     user:process.env.user,
     password:process.env.password,
-    database:process.env.database,
+    database:process.env.database
 
   
 })
