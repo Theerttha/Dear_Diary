22f8195aef7229da1cfc3fe9cd10a324dcf3b397
Theerttha <theerttha.s@gmail.com>
2025-05-14 00:55:06 +0530

    hopefully this works

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
-const mysql=require('mysql');
-
+const mysql=require('mysql2');
 const db= mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
-    password:"RYjeirXIqzyKeUMAVJspGtGnbjgXxmNc",
+    password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
     port:process.env.DB_PORT
-
   
 })
 db.connect(err => {
