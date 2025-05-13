3e65d5ae07758479742a28f23f80a541303499f5
Theerttha <theerttha.s@gmail.com>
2025-05-13 22:08:59 +0530

    variables

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql=require('mysql');
 
 const db= mysql.createConnection({
-    host:process.env.host,
-    user:process.env.user,
-    password:process.env.password,
-    database:process.env.database,
-    port:process.env.port
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT
 
   
 })
