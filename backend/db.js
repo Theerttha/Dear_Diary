d5644fce33e2901fe91b7cfaba16f5aca0e7ec12
Theerttha <theerttha.s@gmail.com>
2025-05-13 21:13:53 +0530

    portAdded

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,8 @@ const db= mysql.createConnection({
     host:process.env.host,
     user:process.env.user,
     password:process.env.password,
-    database:process.env.database
+    database:process.env.database,
+    port:process.env.port
 
   
 })
