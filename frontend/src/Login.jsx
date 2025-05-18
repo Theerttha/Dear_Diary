c838870b9f601a5d8a74b1f44fab333e376062de
Theerttha <theerttha.s@gmail.com>
2025-05-18 22:34:34 +0530

    nameUpdate

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Login.css"; // We'll create this CSS file separately
+import "./login.css"; // We'll create this CSS file separately
 
 const Login = () => {
     const [username, setUsername] = useState("");
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Register.css"; // We'll create this CSS file separately
+import "./register.css"; // We'll create this CSS file separately
 
 const Register = () => {
   const [username, setUsername] = useState("");
